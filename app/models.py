e27dc3f64e6f9bcade0e37d3415c853dfb0804ea
Tito-74 <kipkirui133@gmail.com>
2021-09-19 12:21:20 +0300

    adding methods to class user

--- app/models.py
+++ app/models.py
@@ -26,5 +26,21 @@ class User(UserMixin,db.Model):
 
 
 
-    
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
 
