317effd80cb1f7762534a68d97f98aba40395e37
Tito-74 <kipkirui133@gmail.com>
2021-09-19 12:20:22 +0300

    adding user class

--- app/models.py
+++ app/models.py
@@ -13,5 +13,18 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))  
         
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
 
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+
+
+
+    
 
