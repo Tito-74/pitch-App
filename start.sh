13722902bf2a00f1af1ca71f3d8a7dffca2ef782
Tito-74 <kipkirui133@gmail.com>
2021-09-21 12:12:50 +0300

    initial commit

--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@ export  SECRET_KEY='titokip'
 export MAIL_USERNAME='langatt574@gmail.com'
 export MAIL_PASSWORD='tito@1995'
 python3 manage.py server
+
+# heroku config:set MAIL_USERNAME=langatt574@gmail.com
